60f90cae2fd4ec9c4817f691ec78d5199c1650c2
szellers <scott_zellers@brown.edu>
2015-10-31 23:31:12 -0400

    added colored backgrounds for more pizazz

--- words.js
+++ words.js
@@ -1,22 +1,23 @@
-function Word(text) {
+function Word(text, color) {
 	this.text = text;
+	this.color = color;
 }
 
-var browned = new Word("browned")
-var crowned = new Word("crowned")
-var round = new Word("round")
-var mound = new Word("mound")
-var nouned = new Word("nouned")
-var sound = new Word("sound")
-var pound = new Word("pound")
-var wound = new Word("wound")
-var hound = new Word("hound")
-var astound = new Word("astound")
-var bound = new Word("bound")
-var downed = new Word("downed")
-var ground = new Word("ground")
-var frowned = new Word("frowned")
-var towned = new Word("towned")
+var browned = new Word("browned", "linen")
+var crowned = new Word("crowned", "SeaShell")
+var round = new Word("round", "Moccasin")
+var mound = new Word("mound", "PeachPuff")
+var nouned = new Word("nouned", "MistyRose")
+var sound = new Word("sound", "LightGoldenRodYellow")
+var pound = new Word("pound", "AliceBlue")
+var wound = new Word("wound", "AntiqueWhite")
+var hound = new Word("hound", "Azure")
+var astound = new Word("astound", "Beige")
+var bound = new Word("bound", "BurlyWood")
+var downed = new Word("downed", "Cornsilk")
+var ground = new Word("ground", "DarkSalmon")
+var frowned = new Word("frowned", "Gainsboro")
+var towned = new Word("towned", "LavendarBlush")
 
 setOund(
 	[browned,crowned,round,mound,nouned,sound,pound,wound,hound,astound,bound,downed, ground, frowned, towned]
