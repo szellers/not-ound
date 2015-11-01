86b701854ed0c32ea6b0b70f3904671914cac3ab
szellers <scott_zellers@brown.edu>
2015-10-31 20:11:25 -0400

    made words variables

--- words.js
+++ words.js
@@ -1,4 +1,23 @@
+function Word(text) {
+	this.text = text;
+}
+
+var browned = new Word("browned")
+var crowned = new Word("crowned")
+var round = new Word("round")
+var mound = new Word("mound")
+var nouned = new Word("nouned")
+var sound = new Word("sound")
+var pound = new Word("pound")
+var wound = new Word("wound")
+var hound = new Word("hound")
+var astound = new Word("astound")
+var bound = new Word("bound")
+var downed = new Word("downed")
+var ground = new Word("ground")
+var frowned = new Word("frowned")
+var towned = new Word("towned")
 
 setOund(
-	["browned","crowned","round","mound","nouned","sound","pound","wound","hound","astound","bound","downed, ground, frowned, towned"]
+	[browned,crowned,round,mound,nouned,sound,pound,wound,hound,astound,bound,downed, ground, frowned, towned]
 );
